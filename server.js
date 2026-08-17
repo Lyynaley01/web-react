73b11978c6df115da70c08e11a5bcff28c76c5ce
Lyynaley01 <justlyy256@gmail.com>
2026-08-17 18:13:24 +0700

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ const db = admin.firestore();
 const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname))); // <-- tambahin ini
 
 // ── CONFIG ──
 const REACT_API = "https://api.nexadev.my.id/api/rch";
